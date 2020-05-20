a2d3f372cd0937eb90e1a83522d7b1b9f4a413f6
afif.alfiano <afif.alfiano@uii.ac.id>
2020-05-20 11:13:32 +0700

    add main.yml

--- blog/fourth.md
+++ blog/fourth.md
@@ -1,7 +1,9 @@
 ---
 title: fourth
-description: blog description
+description: 'blog description'
 published: false
----
+slugs:
+    - ___UNPUBLISHED___kaeszhbz_fQAbOUu0IBvjBAOvfYsr4I1YVn8LDooJ
 
+---
 # fourth
--- scully.blog.config.js
+++ scully.blog.config.js
@@ -1,19 +1,17 @@
-const {MinifyHtml} = require('scully-plugin-minify-html');
-const {DisableAngular} = require('scully-plugin-disable-angular');
-
-const postRenderers = [DisableAngular, MinifyHtml];
-
+var MinifyHtml = require('scully-plugin-minify-html').MinifyHtml;
+var DisableAngular = require('scully-plugin-disable-angular').DisableAngular;
+var postRenderers = [DisableAngular, MinifyHtml];
 exports.config = {
-  projectRoot: "./src/app",
-  projectName: "blog",
-  defaultPostRenderers: postRenderers,
-  outDir: "./dist/static",
-  routes: {
-    '/blog/:slug': {
-      type: "contentFolder",
-      slug: {
-        folder: "./blog"
-      },
+    projectRoot: './src/app',
+    projectName: 'blog',
+    defaultPostRenderers: postRenderers,
+    outDir: './dist/static',
+    routes: {
+        '/blog/:slug': {
+            type: 'contentFolder',
+            slug: {
+                folder: './blog'
+            },
+        },
     },
-  },
 };
